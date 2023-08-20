57d83d72a08aa699148b3efa4427123d23eed324
bob johnson <samhammond2017@gmail.com>
2023-08-20 01:02:39 -0700

    first

---
+++ EmployeeManagement/Pages/EmployeeListBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using EmployeeManagementModels;
+
+namespace EmployeeManagement.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+
+        private void LoadEmployees()
+        {
+
+        }
+    }
+}
